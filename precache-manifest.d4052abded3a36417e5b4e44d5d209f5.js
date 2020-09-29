ab8592a78d0d4d43ce93d951b2d8486505786e9d
ABujalance <abujalance@deloittedigital.es>
2020-09-29 23:41:48 +0200

    Updates

---
+++ precache-manifest.d4052abded3a36417e5b4e44d5d209f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ec97564a80b19f5b81aa9bd3b19b0f",
+    "url": "/Awesome-Mapper/index.html"
+  },
+  {
+    "revision": "2202c6a41b557cc34ab1",
+    "url": "/Awesome-Mapper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6ebaad4814b2b3972d43",
+    "url": "/Awesome-Mapper/static/js/2.cf95cf1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Awesome-Mapper/static/js/2.cf95cf1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2202c6a41b557cc34ab1",
+    "url": "/Awesome-Mapper/static/js/main.6faeca9c.chunk.js"
+  },
+  {
+    "revision": "1aa35f0b529e8004a464",
+    "url": "/Awesome-Mapper/static/js/runtime-main.aa9aac9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Awesome-Mapper/static/media/logo.5d5d9eef.svg"
+  }
+]);
