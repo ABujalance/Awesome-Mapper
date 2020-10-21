9d4ab36aef712c932fe012df243a5a2689046243
ABujalance <abujalance@deloittedigital.es>
2020-10-21 20:50:46 +0200

    Updates

--- precache-manifest.4d985d7e06e2cdceefefe961c1dabff3.js
+++ precache-manifest.4d985d7e06e2cdceefefe961c1dabff3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "df5d1bf9a2614e5e41ae19d55c1466be",
+    "revision": "71c8f5af9dfe3c7294ed6efd70e16453",
     "url": "/index.html"
   },
   {
-    "revision": "d293dfa908b8be6adb44",
+    "revision": "a898bc54b287050ecf92",
     "url": "/static/css/2.8ea44cb0.chunk.css"
   },
   {
-    "revision": "9ee26975b56ec9aae8a2",
-    "url": "/static/css/main.e1edd7f6.chunk.css"
+    "revision": "2fd6bd2f95cef3a02946",
+    "url": "/static/css/main.d08f29e4.chunk.css"
   },
   {
-    "revision": "d293dfa908b8be6adb44",
-    "url": "/static/js/2.50306b6d.chunk.js"
+    "revision": "a898bc54b287050ecf92",
+    "url": "/static/js/2.71043d90.chunk.js"
   },
   {
     "revision": "76ea532d20a99de8e0fb888746b5c671",
-    "url": "/static/js/2.50306b6d.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.71043d90.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9ee26975b56ec9aae8a2",
-    "url": "/static/js/main.3156e810.chunk.js"
+    "revision": "2fd6bd2f95cef3a02946",
+    "url": "/static/js/main.d54d0efa.chunk.js"
   },
   {
     "revision": "be556c192baafed41425",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5364895e3a7cbe53a1519d355ee50671.js"
+  "/precache-manifest.4d985d7e06e2cdceefefe961c1dabff3.js"
 );
 
 self.addEventListener('message', (event) => {
